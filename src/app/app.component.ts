ca9c624b744f69c6cda1d2ba22939e7957394194
Denys Andrykevych <denys@wings.ai>
2018-11-11 17:31:09 +0200

    Test web3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import * as Web3 from 'web3';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,25 @@ import { delay } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   title = 'universal-rpc';
-  data: any;
+  accounts: any;
+  web3: any;
+  balance: any;
 
   constructor(private _http: HttpClient) {
+    this.web3 = new Web3();
+    this.web3.setProvider(new Web3.providers.HttpProvider('http://localhost:7545'));
   }
 
   ngOnInit() {
-    this._http.get('https://reqres.in/api/users?delay=5')
-      .subscribe((data) => this.data = data);
+    // this.accounts = this._http.get('http://localhost:3000/blockchain');
+    this.web3.eth.getAccounts((err, accounts) => {
+      if (err === null) {
+        this.accounts = accounts;
+      }
+    });
+
+    this.web3.eth.getBalance('0xf6E28D9F13cb1d53fF955b16713062AC58fe5588', (err, wei) => {
+      this.balance = wei;
+    });
   }
 }
