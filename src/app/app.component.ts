dc64918e6117ad2d023e4fb138f803b4e70b8106
Denys Andrykevych <denys@wings.ai>
2018-11-11 15:12:38 +0200

    Initial universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'universal-rpc';
+  data: any;
+
+  constructor(private _http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this._http.get('https://reqres.in/api/users?delay=5')
+      .subscribe((data) => this.data = data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    HttpClientModule,
+    BrowserModule.withServerTransition({ appId: 'universal-rpc' }),
+    AppRoutingModule,
+    TransferHttpCacheModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
